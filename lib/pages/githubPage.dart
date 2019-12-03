89f009bfcca750c9aa82322bfb7cd36da4c66c23
Miroslav Bartoš <mirek-b@seznam.cz>
2019-12-03 11:30:54 +0100

    Surround list with text and link

--- lib/pages/githubPage.dart
+++ lib/pages/githubPage.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterman/constants.dart';
 import 'package:flutterman/http/githubRepos.dart';
 import 'package:flutterman/http/httpCommunication.dart';
-import 'package:flutterman/pages/listPage.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class GithubPage extends StatefulWidget {
   GithubPage({Key key}) : super(key: key);
@@ -30,51 +31,76 @@ class GithubPageState extends State<GithubPage> {
           future: repos,
           builder: (ctx, snapshot) {
             if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data.repos.length,
-                itemBuilder: (BuildContext ctx, int index) {
-                  // return new Text(snapshot.data.repos[index].name);
-                  return new Column(
-                    children: <Widget>[                      
-                      Row(children: <Widget>[
-                        RichText(
-                          text: TextSpan(
-                            text: '',
-                            style: DefaultTextStyle.of(ctx).style,
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: 'Title: ',
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              TextSpan(
-                                  text: '${snapshot.data.repos[0].getName()}',
-                                  style:
-                                      TextStyle(fontStyle: FontStyle.italic)),
-                            ],
-                          ),
+              return new Column(
+                children: <Widget>[
+                  Text('BartosStore'),
+                  Text(
+                      'This page list all repositories under BartosStore account.'),
+                  Expanded(
+                      child: new ListView.builder(
+                    itemCount: snapshot.data.repos.length,
+                    itemBuilder: (BuildContext ctx, int index) {
+                      return new Column(
+                        children: <Widget>[
+                          Row(children: <Widget>[
+                            RichText(
+                              text: TextSpan(
+                                text: '',
+                                style: DefaultTextStyle.of(ctx).style,
+                                children: <TextSpan>[
+                                  TextSpan(
+                                      text: 'Title: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(
+                                      text:
+                                          '${snapshot.data.repos[0].getName()}',
+                                      style: TextStyle(
+                                          fontStyle: FontStyle.italic)),
+                                ],
+                              ),
+                            ),
+                          ]),
+                          Row(children: <Widget>[
+                            RichText(
+                              text: TextSpan(
+                                text: '',
+                                style: DefaultTextStyle.of(ctx).style,
+                                children: <TextSpan>[
+                                  TextSpan(
+                                      text: 'HTML url: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(
+                                      text: '${snapshot.data.repos[0].htmlUrl}',
+                                      style: TextStyle(
+                                          fontStyle: FontStyle.italic)),
+                                ],
+                              ),
+                            ),
+                          ]),
+                        ],
+                      );
+                    },
+                  )),
+                  RichText(
+                    text: new TextSpan(
+                      children: [
+                        new TextSpan( 
+                          text: "If you want to know more, check this: ",
+                          style: new TextStyle(color: Colors.black),
                         ),
-                      ]),
-                      Row(children: <Widget>[
-                        RichText(
-                          text: TextSpan(
-                            text: '',
-                            style: DefaultTextStyle.of(ctx).style,
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: 'HTML url: ',
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              TextSpan(
-                                  text: '${snapshot.data.repos[0].htmlUrl}',
-                                  style:
-                                      TextStyle(fontStyle: FontStyle.italic)),
-                            ],
-                          ),
-                        ),
-                      ])
-                    ],
-                  );
-                },
+                        new TextSpan(
+                          text: "BartosStore",
+                          style: new TextStyle(color: Colors.blue),
+                          recognizer: new TapGestureRecognizer()
+                            ..onTap = () { launch('https://github.com/BartosStore/');
+                          },
+                        )
+                      ]
+                    ),
+                  )
+                ],
               );
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
