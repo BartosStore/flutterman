a56dbcad27fe970b0d4dc76b946b2a57ccd832d8
Miroslav Bartoš <mirek-b@seznam.cz>
2019-12-03 10:49:34 +0100

    Display title and url of each github repo

--- lib/pages/githubPage.dart
+++ lib/pages/githubPage.dart
@@ -23,63 +23,65 @@ class GithubPageState extends State<GithubPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("BartosStore Github Repos"),
-      ),
-      body: FutureBuilder<ReposList>(
-        future: repos,
-        builder: (ctx, snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    RichText(
-                      text: TextSpan(
-                        text: '',
-                        style: DefaultTextStyle.of(ctx).style,
-                        children: <TextSpan>[
-                          TextSpan(text: 'Title: ', style: TextStyle(fontWeight: FontWeight.bold)),
-                          TextSpan(text: '${snapshot.data.repos[0].getName()}', style: TextStyle(fontStyle: FontStyle.italic)),
-                        ],
-                      ),
-                    ),
-                  ]
-                ),
-                Row(
-                  children: <Widget>[
-                    RichText(
-                      text: TextSpan(
-                        text: '',
-                        style: DefaultTextStyle.of(ctx).style,
-                        children: <TextSpan>[
-                          TextSpan(text: 'Response body: ', style: TextStyle(fontWeight: FontWeight.bold)),
-                          TextSpan(text: '${snapshot.data.repos[0].htmlUrl}', style: TextStyle(fontStyle: FontStyle.italic)),
-                        ],
-                      ),
-                    ),
-                  ]
-                ),
-                RaisedButton(
-                  child: Text("Go to Contact Page"),
-                  onPressed: () {
-                    Navigator.push(
-                      context, 
-                      MaterialPageRoute(builder: (context) => ListPage(title: 'Text'))
-                    );
-                  },
-                )
-              ]
-            );
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
+        appBar: AppBar(
+          title: Text("BartosStore Github Repos"),
+        ),
+        body: FutureBuilder<ReposList>(
+          future: repos,
+          builder: (ctx, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.repos.length,
+                itemBuilder: (BuildContext ctx, int index) {
+                  // return new Text(snapshot.data.repos[index].name);
+                  return new Column(
+                    children: <Widget>[                      
+                      Row(children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            text: '',
+                            style: DefaultTextStyle.of(ctx).style,
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: 'Title: ',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(
+                                  text: '${snapshot.data.repos[0].getName()}',
+                                  style:
+                                      TextStyle(fontStyle: FontStyle.italic)),
+                            ],
+                          ),
+                        ),
+                      ]),
+                      Row(children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            text: '',
+                            style: DefaultTextStyle.of(ctx).style,
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: 'HTML url: ',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(
+                                  text: '${snapshot.data.repos[0].htmlUrl}',
+                                  style:
+                                      TextStyle(fontStyle: FontStyle.italic)),
+                            ],
+                          ),
+                        ),
+                      ])
+                    ],
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
 
-          return CircularProgressIndicator();
-        },
-      )
-    );
+            return CircularProgressIndicator();
+          },
+        ));
   }
 }
