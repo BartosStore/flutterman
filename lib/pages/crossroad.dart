bd083b0489053df29fa32dca3c4a11bea92d138b
Miroslav Bartoš <mirek-b@seznam.cz>
2019-12-09 12:24:20 +0100

    Style app menu into tiles

--- lib/pages/crossroad.dart
+++ lib/pages/crossroad.dart
@@ -24,65 +24,112 @@ class CrossroadState extends State<Crossroad> {
 
   @override
   Widget build(BuildContext context) {
+    Size screen = MediaQuery.of(context).size;
+    double buttonSize = (screen.width / 3);
+
     return Scaffold(
         appBar: AppBar(
           title: Text("Contact detail"),
         ),
         body: Column(
           children: <Widget>[
-            RaisedButton(
-              child: Text("Contact Page"),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ListPage(
-                              title: 'Contacts',
-                            )));
-              },
-            ),
-            RaisedButton(
-              child: Text("Net Page"),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => NetPage()));
-              },
-            ),
-            RaisedButton(
-              child: Text("Github Page"),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => GithubPage()));
-              },
-            ),
-            FutureBuilder(
-              future: _camera,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return RaisedButton(
-                    child: Text("Camera"),
+            Row(
+              children: <Widget>[
+                Container(
+                  height: buttonSize,
+                  width: buttonSize,
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.lightGreen)),
+                  child: RaisedButton(
+                    child: Text("Contact Page"),
+                    color: Colors.green,
                     onPressed: () {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => TakePictureScreen(
-                                    camera: snapshot.data,
+                              builder: (context) => ListPage(
+                                    title: 'Contacts',
                                   )));
                     },
-                  );
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
+                  ),
+                ),
+                Container(
+                  height: buttonSize,
+                  width: buttonSize,
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.lightGreen)),
+                  child: RaisedButton(
+                    child: Text("Net Page"),
+                    color: Colors.green,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => NetPage()));
+                    },
+                  ),
+                ),
+                Container(
+                  height: buttonSize,
+                  width: buttonSize,
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.lightGreen)),
+                  child: RaisedButton(
+                    child: Text("Github Page"),
+                    color: Colors.green,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => GithubPage()));
+                    },
+                  ),
+                )
+              ],
             ),
-            RaisedButton(
-              child: Text('QR Code'),
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => QrCode()));
-              },
+            Row(
+              children: <Widget>[
+                Container(
+                  height: buttonSize,
+                  width: buttonSize,
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.lightGreen)),
+                  child: FutureBuilder(
+                    future: _camera,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return RaisedButton(
+                          child: Text("Camera"),
+                          color: Colors.green,
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => TakePictureScreen(
+                                          camera: snapshot.data,
+                                        )));
+                          },
+                        );
+                      } else {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    },
+                  ),
+                ),
+                Container(
+                    height: buttonSize,
+                    width: buttonSize,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.lightGreen)),
+                    child: RaisedButton(
+                      child: Text('QR Code'),
+                      color: Colors.green,
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => QrCode()));
+                      },
+                    ))
+              ],
             )
           ],
         ));
