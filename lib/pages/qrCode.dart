dd78d6b45403b68d2735748a3bcec82e69be9fa8
Miroslav Bartoš <mirek-b@seznam.cz>
2019-12-09 18:51:24 +0100

    Scan QR code

--- lib/pages/qrCode.dart
+++ lib/pages/qrCode.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 
-class QrCode extends StatelessWidget {
+class QrCode extends StatefulWidget {
+  @override
+  QrCodeState createState() => QrCodeState();
+}
+
+class QrCodeState extends State<QrCode> {
+  String _code;
+
+  void _scanCode() async {
+    try {
+      String code = await BarcodeScanner.scan();
+      this.setState(() {
+        this._code = code;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('QR Code'),),
-      body: Text('Here we will scan QR codes...'),
+      appBar: AppBar(
+        title: Text('QR Code'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Code: ${this._code}'),
+          RaisedButton.icon(
+            icon: Icon(Icons.scanner),
+            label: Text('Scan code'),
+            onPressed: _scanCode,
+            color: Colors.green,
+            textColor: Colors.white,
+          ),
+        ],
+      )),
     );
   }
 }
