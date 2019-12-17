2b69fce674b2ccb572c6416a85a5c2eb7bb3bdc9
Miroslav Bartoš <mirek-b@seznam.cz>
2019-12-17 15:33:12 +0100

    Prepare widget for GraphQl testing

--- lib/pages/graphql.dart
+++ lib/pages/graphql.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class Graphql extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Text('data');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GraphQl testing'),
+      ),
+      body: Text('TBD'),
+    );
   }
 }
